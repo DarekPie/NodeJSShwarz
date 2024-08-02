3a4589a16c7fb92c12fd72382cb184ab70b3047c
Dariusz Piechota <dapi@gmv.com>
2024-08-02 09:56:57 +0200

    Express addon middleware ading

--- app node/app.js
+++ app node/app.js
@@ -1,21 +1,20 @@
-// const http = require('http');
-// const routes = require('./route');  - wywalamy bo express
-const express = require('express');  
-
-
-// function rqListener(req, res){
-// }
-// const server = http.createServer((req,res) => {
-//     console.log(req, req.method, req.headers);
-// });
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res, next) => {
-    console.log('In the another middleware');
-    res.send('<h1>Hello from express</h1>');
-    //
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+
 });
 
 
-server.listen(3000);
+app.listen(3000);
