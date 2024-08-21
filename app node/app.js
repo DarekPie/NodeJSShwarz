2d59b23462b4c7d2e58c3d129c1001582cf93fee
Dariusz Piechota <dapi@gmv.com>
2024-08-21 14:48:08 +0200

    Command another

--- app node/app.js
+++ app node/app.js
@@ -1,20 +1,27 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs'); 
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>');
-
+app.use((req, res, next) => {
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
-
 
 app.listen(3000);
--- app node/routes/admin.js
+++ app node/routes/admin.js
@@ -2,15 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-//   res.send(
-//     '<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-//   );
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add Product', path:'/admin/add-product'})
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
